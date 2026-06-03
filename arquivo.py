f22cfd25508349f279d17d6f9487e8e928deddce
Gabriel <gmullergabe@gmail.com>
2026-06-03 02:31:05 -0300

    Adicionando o sistema de datas.

--- arquivo.py
+++ arquivo.py
@@ -5,7 +5,8 @@ def salvar_tarefas(tarefas, nome_arquivo="tarefas.json"):
     for tarefa in tarefas:
         dados.append({
             "descricao": tarefa.descricao,
-            "concluida": tarefa.concluida
+            "concluida": tarefa.concluida,
+            "data": tarefa.data
         })
     with open(nome_arquivo, "w", encoding="utf-8") as f:
         json.dump(dados, f, indent=4, ensure_ascii=False)
@@ -19,6 +20,7 @@ def carregar_tarefas(nome_arquivo="tarefas.json"):
             for item in dados:
                 tarefa = atividade.Tarefa(item["descricao"])
                 tarefa.concluida = item["concluida"]
+                tarefa.data = item["data"]
                 tarefas.append(tarefa)
             return tarefas
     except FileNotFoundError:
--- atividade.py
+++ atividade.py
@@ -1,8 +1,11 @@
+from datetime import datetime
+
 class Tarefa():
 
     def __init__(self, descricao):
         self.descricao = descricao
         self.concluida = False
+        self.data = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 
     def concluir(self):
@@ -11,7 +14,7 @@ class Tarefa():
     
     def __str__(self):
             if self.concluida == True:
-                status = f'[X] {self.descricao}'
+                status = f'[X] {self.descricao}, adicionada em {self.data}'
             else:
-                status = f'[ ] {self.descricao}'
+                status = f'[ ] {self.descricao}, adicionada em {self.data}'
             return status
